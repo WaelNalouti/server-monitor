47850c10eb7fce9966efa6717f653cd2ad915e82
WaelNalouti <Wael99g1@gmail.com>
2021-08-12 14:39:23 +0100

    first commit

--- backend/DAO/MemoryDAO.js
+++ backend/DAO/MemoryDAO.js
@@ -1,28 +1,28 @@
-import os from "os"
+import os from "os";
 
 let RAM = {
-    free: null,
-    total: null,
-    percentMem: null
-}
+  free: null,
+  total: null,
+  percentMem: null,
+};
 
 export default class MemoryDAO {
-    static async getMemoryInfo() {
+  static async getMemoryInfo() {
+    try {
+      RAM.free = (await (os.freemem() / 1024 / 1024 / 1024).toFixed(2)) + " GB";
+      RAM.total =
+        (await (os.totalmem() / 1024 / 1024 / 1024).toFixed(2)) + " GB";
 
-        try {
-            RAM.free = await (os.freemem()/1024/1024/1024).toFixed(2) + " GB";
-            RAM.total = await (os.totalmem()/1024/1024/1024).toFixed(2) + " GB";
-
-            if (RAM.free !== null && RAM.total !== null) {
-               RAM.percentMem = ((os.freemem() * 100) / os.totalmem()).toFixed(2)+"%";
-                
-            }   else {
-                return;
-            }
-            return RAM;
-            
-        } catch (error) {
-            console.log("Cannot access memory data");
-        }
-    };
+      if (RAM.free !== null && RAM.total !== null) {
+        RAM.percentMem =
+          (((os.totalmem() - os.freemem()) * 100) / os.totalmem()).toFixed(2) +
+          "%";
+      } else {
+        return;
+      }
+      return RAM;
+    } catch (error) {
+      console.log("Cannot access memory data");
+    }
+  }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,58 +1,49 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
 import MemoryDAO from "./DAO/MemoryDAO.js";
 import DiskDAO from "./DAO/DiskDAO.js";
-import os from "os"
+import os from "os";
 
-
-const app = express()
+const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 //applying middleware
 app.use(express.json()); //the server can accept json in the body of the request
 
-
-
-
-
 // let RAM;
 // let DISK;
-let stats =  {
-    host: "",
-    os:"",
-    RAM: {},
-    DISK: {},
+let stats = {
+  host: "",
+  os: "",
+  networks: {},
+  RAM: {},
+  DISK: {},
 };
 
-stats.host = os.hostname(),
-stats.os = os.platform() + " @ " +  os.release(),
-await MemoryDAO.getMemoryInfo().then((memory) => {
+stats.host = os.hostname();
+stats.os = os.platform() + " @ " + os.release();
+stats.networks = os.networkInterfaces();
+
+await MemoryDAO.getMemoryInfo().then(
+  (memory) => {
     stats.RAM = {
-        type:"RAM",
-        memory,
+      type: "RAM",
+      memory,
     };
-},
-await DiskDAO.getDiskInfo().then((disk) => {
+  },
+  await DiskDAO.getDiskInfo().then((disk) => {
     stats.DISK = {
-        type: "DISK",
-        disk,
+      type: "DISK",
+      disk,
     };
-}),
+  }),
 
-
-app.get('/api/v1/stats', (req, res) => {
-  res.json(stats)
-})
-
-)
+  app.get("/api/v1/stats", (req, res) => {
+    res.json(stats);
+  })
+);
 app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`);
   console.log(stats);
-})
-
-
-
-
-
-
+});
